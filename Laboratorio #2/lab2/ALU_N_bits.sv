9228f667354ab4abc3d09112063237c616c79275
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2025-03-24 21:28:12 -0600

    [ADD] Creacion de proyecto
    Se anade codigo inicial para mux 4:1 usando mux 2:1. Se reune el grupo de trabajo para planificar el proyecto.
    
    Co-Authored-By: Alexander Montero Vargas <142643937+Alexmv235@users.noreply.github.com>
    Co-Authored-By: José Bernardo Barquero Bonilla <131917809+JBB092@users.noreply.github.com>

---
+++ Laboratorio #2/lab2/ALU_N_bits.sv
@@ -0,0 +1,46 @@
+module ALU_N_bits 
+	#(parameter N=4)
+	 (input logic[N-1:0] a, b,
+	  input logic[1:0] control, // 2 bits for 4 op, need 
+	  output logic[N-1:0] result,
+	  output logic v, c, n, z);
+	// Wires
+	
+	// Operations
+		// OR
+		// AND
+		// Add
+		1. codigo
+			- maquina (ALU)
+			- operaciones
+				and 
+				or 
+				- 
+				+ 
+				
+				*
+				/
+				%
+				<<
+				>>
+				xor
+				
+				xor
+		
+		
+		2. timming
+			- Leer
+			
+		
+		// Sub
+		or()
+		and()
+		xor(a,b)
+	// Mux
+	
+	// Flags
+	
+
+
+
+endmodule
