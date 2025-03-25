5588990f9c4877e5ebbf5047345066e891ffe3fd
Álvaro Vargas Molina <36602950+AlvaroVargasM@users.noreply.github.com>
2025-03-25 05:06:06 -0600

    [UPDATE] Desarrollo de ALU 4 operaciones
    Se armo la base del ALU para cuatro operaciones, con sus flags.

--- Laboratorio #2/lab2/ALU_N_bits.sv
+++ Laboratorio #2/lab2/ALU_N_bits.sv
@@ -1,46 +1,40 @@
 module ALU_N_bits 
 	#(parameter N=4)
 	 (input logic[N-1:0] a, b,
-	  input logic[1:0] control, // 2 bits for 4 op, need 
+	  input logic[1:0] control, // Need 4 bits to select from 10 operations
 	  output logic[N-1:0] result,
 	  output logic v, c, n, z);
+	
 	// Wires
-	
+
+	logic[N-1:0] b_sum, res_sum, res_and, res_or;
+	logic sum_carry;
+	assign b_sum = control[0] ? ~b : b;
+		
 	// Operations
-		// OR
-		// AND
-		// Add
-		1. codigo
-			- maquina (ALU)
-			- operaciones
-				and 
-				or 
-				- 
-				+ 
-				
-				*
-				/
-				%
-				<<
-				>>
-				xor
-				
-				xor
+
+	assign res_and = a & b;
+	assign res_or = a | b;	
+	sum#(.N(N)) alu_sum(.a(a),
+							  .b(b_sum),
+							  .cin(control[0]),
+							  .s(res_sum),
+							  .cout(sum_carry));
 		
+	// Multiplexer
 		
-		2. timming
-			- Leer
-			
-		
-		// Sub
-		or()
-		and()
-		xor(a,b)
-	// Mux
-	
+	mux4#(.N(N)) alu_controller(.d0(res_sum), 
+										 .d1(res_sum),
+										 .d2(res_and),
+										 .d3(res_or),
+										 .s(control),
+										 .y(result)); 									 
+										 
 	// Flags
 	
-
-
+	assign z = &(~result);
+	assign n = result[N-1];
+	assign c = ~control[1] & sum_carry;
+	assign v = ~(control[1] ^ a[N-1] ^ b[N-1]) & (a[N-1] ^ res_sum[N-1]) & ~control[1];
 
 endmodule
--- Laboratorio #2/lab2/mux4.sv
+++ Laboratorio #2/lab2/mux4.sv
@@ -1,12 +1,17 @@
+// Pg. 249 H&H
+/* s = 00 => d0 ADD
+	s = 01 => d1 SUB
+	s = 10 => d2 AND
+	s = 11 => d3 OR
+*/ 
+
+// Pg. 182 H&H
 module mux4
-	#(parameter width=4)
-	 (input logic [width-1:0] d0, d1, d2, d3,
+	#(parameter N=4)
+	 (input logic [N-1:0] d0, d1, d2, d3,
 	  input logic [1:0] s,
-     output logic [width-1:0] y);
-	// Internal wires
-	logic [width-1:0] low, hi;
-	// Using three 2;1 mux to create a 4:1 mux
-	mux2 lowmux(d0, d1, s[0], low);
-	mux2 himux(d2, d3, s[0], hi);
-	mux2 outmux(low, hi, s[1], y);
+	  output logic [N-1:0] y);
+// NEED TO CHANGE TO N, WIP
+assign y = s[1] ? (s[0] ? d3 : d2)
+: (s[0] ? d1 : d0);
 endmodule
